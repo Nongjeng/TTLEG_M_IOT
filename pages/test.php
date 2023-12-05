36ed777a9e8ff8b4c0a04d28a313654bc3bb286b
Nongjeng <65309010007@tatc.ac.th>
2023-12-05 17:32:19 +0700

    Test2

--- pages/test.php
+++ pages/test.php
@@ -1,3 +1,10 @@
+<?php
+// Retrieve the data sent by ESP32
+$sensorValue = $_POST['sensor_value'];
+
+// Process the data (you can do whatever you want with $sensorValue)
+echo "Received sensor value: " . $sensorValue;
+?>
 
 <body>
 welcome !!
